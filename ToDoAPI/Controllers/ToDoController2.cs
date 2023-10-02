83590c3ea847ae6ceb197773e388b0007b6b9a7c
Ryuyuyuyu <124693538+Ryuyuyuyu@users.noreply.github.com>
2023-10-02 14:16:24 +0200

    todo api werkt

---
+++ ToDoAPI/Controllers/ToDoController2.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoListModel;
+using ToDoListModel.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController2 : ControllerBase
+    {
+        // GET: api/<ToDoController2>
+        [HttpGet]
+        public IEnumerable<ToDoTask> Get()
+        {
+            return ToDoTask.ReadAll();
+        }
+
+        // GET api/<ToDoController2>/5
+        [HttpGet("{id}")]
+        public ToDoTask Get(int id)
+        {
+            return ToDoTask.Read(id);
+        }
+
+        // POST api/<ToDoController2>
+        [HttpPost]
+        public void Post([FromBody] string description)
+        {
+            ToDoTask newTask = new ToDoTask(description);
+            newTask.Create();
+        }
+
+        // PUT api/<ToDoController2>/5
+        [HttpPut("{id}")]
+        public void Put(int id)
+        {
+            ToDoTask finishedTask = ToDoTask.Read(id);
+            finishedTask.FinishTask();
+        }
+
+        // DELETE api/<ToDoController2>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            ToDoTask deleteTask = ToDoTask.Read(id);
+            deleteTask.Delete();
+        }
+    }
+}
